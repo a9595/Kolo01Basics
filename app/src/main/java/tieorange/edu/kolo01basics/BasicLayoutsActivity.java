09cbeea1d05d23eb7bbc1c2732a396c57871726d
stud <stud@Michas-iMac.local>
2016-11-16 15:24:00 +0100

    Grid

---
+++ app/src/main/java/tieorange/edu/kolo01basics/BasicLayoutsActivity.java
@@ -0,0 +1,13 @@
+package tieorange.edu.kolo01basics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicLayoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_layouts);
+    }
+}
